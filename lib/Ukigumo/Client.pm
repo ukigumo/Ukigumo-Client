07510efc9038b07d763034101cf692c06c96c8c4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-09-03 19:15:04 +0900

    initial import

---
+++ lib/Ukigumo/Client.pm
@@ -0,0 +1,39 @@
+package Ukigumo::Client;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Ukigumo::Client -
+
+=head1 SYNOPSIS
+
+  use Ukigumo::Client;
+
+=head1 DESCRIPTION
+
+Ukigumo::Client is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
