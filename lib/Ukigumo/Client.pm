4694822ead8e4fb1594010a3b7cf4d61acfb8f06
moznion <moznion@gmail.com>
2014-03-17 15:07:52 +0900

    Oops, the bug is at conditional branch...

--- lib/Ukigumo/Client.pm
+++ lib/Ukigumo/Client.pm
@@ -191,7 +191,7 @@ sub _load_notifications {
         if ($type eq 'ikachan') {
             $self->_load_notify_modules($conf, $type, NOTIFIER_IKACHAN);
         }
-        if ($type eq 'github_statuses') {
+        elsif ($type eq 'github_statuses') {
             $self->_load_notify_modules($conf, $type, NOTIFIER_GITHUBSTATUSES);
         } else {
             die "Unknown notification type: $type";
